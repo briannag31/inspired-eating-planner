c2b8e92fae08d08c4cee072bac73eb89cd4f12a7
Brianna Gaines <briannagaines31@gmail.com>
2022-04-26 18:39:13 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -30,9 +30,5 @@ app.post(function(req, res, next){
     next();
 });
 
-
-   
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 app.listen(PORT, () => console.log("we are running"))
